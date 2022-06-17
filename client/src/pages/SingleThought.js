a5fa61c2a8e048fb8b6c962d97308bcceb45f80d
Cole <deoncole77@gmail.com>
2022-06-17 09:53:00 -0400

    added routes

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,41 @@
 import React from 'react';
+// import the useParams hook from the package to pass info from the searh bar
+import { useParams } from 'react-router-dom';
+//import the use query hook
+import { useQuery } from '@apollo/client';
+//import the query to the database for the single thought
+import { QUERY_THOUGHT } from '../utils/queries';
+// import the reaction list page to be able to add/see reactions to a thought
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+
+  const { id: thoughtId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}
           </span>{' '}
-          thought on createdAt
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
